757dc6308bda0d8a7cb156bcd390edb8cdf197cb
alexSchwitthal <schwitthal.alexandre@gmail.com>
2020-12-13 16:31:00 +0100

    nothing

--- public/js/notes.js
+++ public/js/notes.js
@@ -63,19 +63,19 @@ $(document).ready(function() {
 			else {
 				console.log(this.style.backgroundColor);
 				this.style.display = "none";
-				if(filter.includes("green") && this.style.backgroundColor ==="rgb(134, 247, 137)"){					
+				if(filter.includes("green") && this.style.backgroundColor === "rgb(134, 247, 137)") {					
 					this.style.display = "block";
 				}
-				else if (filter.includes("yellow") && this.style.backgroundColor ==="rgb(246, 255, 122)"){
+				else if (filter.includes("yellow") && this.style.backgroundColor === "rgb(246, 255, 122)") {
+					this.style.display = "block";
+				} 
+				else if (filter.includes("blue") && this.style.backgroundColor === "rgb(107, 188, 242)") {
 					this.style.display = "block";
 				}
-				else if (filter.includes("blue") && this.style.backgroundColor ==="rgb(107, 188, 242)"){
+				else if (filter.includes("orange") && this.style.backgroundColor === "rgb(245, 175, 91)") {
 					this.style.display = "block";
 				}
-				else if (filter.includes("orange") && this.style.backgroundColor ==="rgb(245, 175, 91)"){
-					this.style.display = "block";
-				}
-				else if (filter.includes("red") && this.style.backgroundColor ==="rgb(242, 107, 107)"){
+				else if (filter.includes("red") && this.style.backgroundColor === "rgb(242, 107, 107)") {
 					this.style.display = "block";
 				}
 			}
@@ -85,7 +85,7 @@ $(document).ready(function() {
 	$('#filtresList').on('click', 'li', (function() {
 		// Clic sur le filtre blanc qui efface tous les filtres enregistrés
 		if(this.id == "flitreAll") {
-			while(document.getElementsByClassName("activeFilter").length!=0){
+			while(document.getElementsByClassName("activeFilter").length != 0) { 
 				let activeFilters = document.getElementsByClassName("activeFilter");
 				for (let index = 0; index < activeFilters.length; ++index) {
 					activeFilters[index].style.borderStyle = "dotted";
@@ -96,7 +96,7 @@ $(document).ready(function() {
 		}
 		else {
 			// Modification de l'icone du filtre
-			if (this.classList.contains("activeFilter")){
+			if(this.classList.contains("activeFilter")) {
 				this.classList.remove("activeFilter");
 				this.style.borderStyle = "dotted";				
 			}
@@ -106,7 +106,7 @@ $(document).ready(function() {
 			}
 
 			var colorList = ["blue", "green", "yellow", "orange", "red"];
-			
+
 			for(var color of colorList) {
 				var elementId = "filtre" + color.charAt(0).toUpperCase() + color.slice(1);
 				if(document.getElementById(elementId).classList.contains("activeFilter")) {
